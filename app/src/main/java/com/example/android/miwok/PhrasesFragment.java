e99a7c843b53813e257b6a426a256470f8a56561
Bruno Mendoza <hola@brunomendoza.es>
2017-10-25 15:46:53 +0100

    Family members fragment done.

---
+++ app/src/main/java/com/example/android/miwok/PhrasesFragment.java
@@ -0,0 +1,13 @@
+package com.example.android.miwok;
+
+import android.app.Fragment;
+
+/**
+ * Created by bruno on 25/10/17.
+ */
+
+public class PhrasesFragment extends Fragment {
+    public PhrasesFragment() {
+        super();
+    }
+}
