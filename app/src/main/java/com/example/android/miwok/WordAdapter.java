8cf1ea9c8d9e91840d91643e8efdf5af1af68c47
Bruno Mendoza <hola@brunomendoza.es>
2017-10-25 01:27:10 +0100

    Number category running successfully.

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,7 +1,6 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -10,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -61,7 +59,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView miwokTranslationTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTranslationTextView.setText(currentWord.getMiwokTranslation());
 
-        View tranlationWrapper = listItemView.findViewById(R.id.translation_wrapper);
+        View tranlationWrapper = listItemView.findViewById(R.id.text_container);
         int color = ContextCompat.getColor(getContext(), mColorResourceId);
         tranlationWrapper.setBackgroundColor(color);
 
